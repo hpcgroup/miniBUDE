5585e28e09e1d9289783628799fbadd27a549687
Pranav Sivaraman <psivaram@umd.edu>
2024-03-11 18:31:42 -0400

    refactor(hip): use enable langague HIP

--- src/hip/model.cmake
+++ src/hip/model.cmake
@@ -1,7 +1,6 @@
-
-register_flag_required(CMAKE_CXX_COMPILER
-        "Absolute path to the AMD HIP C++ compiler")
-
 macro(setup)
     # nothing to do here as hipcc does everything correctly, what a surprise!
+    enable_language(HIP)
+    set(CMAKE_HIP_STANDARD 17)
+    set_source_files_properties(src/main.cpp PROPERTIES LANGUAGE HIP)
 endmacro()
